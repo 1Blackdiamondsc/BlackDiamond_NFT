822dc46a975092ca1912bed874aa2bbcba4723c0
BlackDiamond SC Inc [BDSCI TOKEN] <73549208+1Blackdiamondsc@users.noreply.github.com>
2021-07-10 21:01:13 -0400

    Add files via upload

---
+++ inufakey.js
@@ -0,0 +1,4 @@
+curl https://<network>.infura.io/v3/4b6f463c8f9e464abbeb9ad02c94cc31 \
+    -X POST \
+    -H "Content-Type: application/json" \
+    -d '{"jsonrpc":"2.0","method":"eth_accounts","params":[],"id":1}'
